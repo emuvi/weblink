34a92a78be450dd07811e0f4e1afefd1b05f0214
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-01 13:34:48 -0300

    prototype development

--- main.go
+++ main.go
@@ -3,36 +3,58 @@ package main
 import (
 	"io"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	resp, err := http.Get("https://www.uol.com.br/")
-	if err != nil {
-		panic(err)
-	}
-	links := parse(resp.Body)
-	for _, link := range links {
-		println(link)
+	for i, link := range os.Args {
+		if i == 0 {
+			continue
+		}
+		for _, l := range process(link) {
+			println(l)
+		}
 	}
 }
 
-func parse(reader io.ReadCloser) (links []string) {
-	defer reader.Close()
-	var result []string
-	tkns := html.NewTokenizer(reader)
+func process(link string) []string {
+	resp, err := http.Get(link)
+	if err != nil {
+		panic(err)
+	}
+	links := getLinks(resp.Body)
+	url, err := url.Parse(link)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < len(links); i++ {
+		if !(strings.HasPrefix(links[i], "http:/") || strings.HasPrefix(links[i], "https:/")) {
+			links[i] = path.Join(url.Host, url.Path, links[i])
+		}
+	}
+	return links
+}
+
+func getLinks(fromReader io.ReadCloser) []string {
+	defer fromReader.Close()
+	var results []string
+	tkns := html.NewTokenizer(fromReader)
 	for {
 		tknType := tkns.Next()
 		switch {
 		case tknType == html.ErrorToken:
-			return result
+			return results
 		case tknType == html.StartTagToken:
 			tkn := tkns.Token()
 			if tkn.Data == "a" {
 				for _, attr := range tkn.Attr {
 					if attr.Key == "href" {
-						result = append(result, attr.Val)
+						results = append(results, attr.Val)
 					}
 				}
 			}
