a5b7ee8614c1e7909705b1f90e47bd38ad5675c7
Éverton M. Vieira <everton.muvi@gmail.com>
2022-05-01 16:09:44 -0300

    prototype development

--- main.go
+++ main.go
@@ -5,42 +5,76 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path"
+	"strconv"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
+var until_depth = 3
+var follow_external = false
+
 func main() {
-	for i, link := range os.Args {
-		if i == 0 {
-			continue
+	links := make(map[string]int)
+	index := 1
+	length := len(os.Args)
+	var err error
+	for index < length {
+		if os.Args[index] == "-d" || os.Args[index] == "--depth" {
+			until_depth, err = strconv.Atoi(os.Args[index+1])
+			if err != nil {
+				panic(err)
+			}
+			index += 2
+		} else {
+			links[strings.ToLower(os.Args[index])] = 1
+			index++
 		}
-		for _, l := range process(link) {
-			println(l)
+	}
+	actual_depth := 1
+	for actual_depth <= until_depth {
+		for link, link_depth := range links {
+			if link_depth == actual_depth {
+				for _, new_link := range parse(link) {
+					links[new_link] = actual_depth + 1
+				}
+			}
 		}
+		actual_depth += 1
+	}
+	for link := range links {
+		println(link)
 	}
 }
 
-func process(link string) []string {
-	resp, err := http.Get(link)
+func parse(root string) []string {
+	resp, err := http.Get(root)
 	if err != nil {
 		panic(err)
 	}
-	links := getLinks(resp.Body)
-	url, err := url.Parse(link)
+	links := catch(resp.Body)
+	urlRoot, err := url.Parse(root)
 	if err != nil {
 		panic(err)
 	}
-	for i := 0; i < len(links); i++ {
-		if !(strings.HasPrefix(links[i], "http:/") || strings.HasPrefix(links[i], "https:/")) {
-			links[i] = path.Join(url.Host, url.Path, links[i])
+	var results []string
+	for _, link := range links {
+		urlLink, err := url.Parse(link)
+		if err != nil {
+			panic(err)
+		}
+		if !urlLink.IsAbs() {
+			urlLink = urlRoot.ResolveReference(urlLink)
+		}
+
+		if strings.HasPrefix(link, root) {
+			results = append(results, link)
 		}
 	}
-	return links
+	return results
 }
 
-func getLinks(fromReader io.ReadCloser) []string {
+func catch(fromReader io.ReadCloser) []string {
 	defer fromReader.Close()
 	var results []string
 	tkns := html.NewTokenizer(fromReader)
@@ -54,7 +88,7 @@ func getLinks(fromReader io.ReadCloser) []string {
 			if tkn.Data == "a" {
 				for _, attr := range tkn.Attr {
 					if attr.Key == "href" {
-						results = append(results, attr.Val)
+						results = append(results, strings.ToLower(attr.Val))
 					}
 				}
 			}
